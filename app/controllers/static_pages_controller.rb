37ac8fac7e05fcf2d031d08aea9d4f8bfbae719c
saul <saul@saul-desktop.(none)>
2015-02-24 15:35:07 -0400

    aplicación de stylo css y bootstrap

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,11 @@
 class StaticPagesController < ApplicationController
   def home
+    @algo = %W(lunes martes miercoles jueves viernes)
+    @algo2=%W(saul carlos david)
   end
 
   def help
+
   end
+
 end
---
+++ test/controllers/saul_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SaulControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
